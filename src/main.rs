0357ceea72649cff32735e7d59c4aa9d407caf26
TheMasonX <norrtec@gmail.com>
2023-11-24 00:10:16 -0600

    Fixed the formatting to handle all cases

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,32 @@ macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);
 }
 
+// macro_rules! unwrap_or_unknown {
+//     ($($x:expr)) => {
+//         $x.unwrap_or(String::from("Unknown"))
+//     };
+// }
+
 fn main() {
     let mut lukes_loves: Vec<String> = vec_of_strings!["Nona", "Samantha", "Lucy", "Charles"];
-    lukes_loves.push("Myself".to_string());
+    lukes_loves.push("myself".to_string());
     print_my_love(lukes_loves);
 }
 
 fn print_my_love(my_loves: Vec<String>) {
-    let string_val: String = my_loves.join(", ");
+    let string_val: String = match my_loves.len() {
+        0 => "nobody".to_string(),
+        1 => my_loves.concat().to_string(),
+        2 => format!(
+            "{} and {}",
+            my_loves.first().unwrap(),
+            my_loves.last().unwrap(),
+        ),
+        _ => format!(
+            "{}, and {}",
+            my_loves[..my_loves.len() - 1].join(", "),
+            my_loves.last().unwrap(),
+        ),
+    };
     println!("I love {}!", string_val);
 }
