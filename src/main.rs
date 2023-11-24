b6e805e170527c5c55a4e7353350c189bff8a8a1
TheMasonX <norrtec@gmail.com>
2023-11-23 23:21:31 -0600

    Played with macro, mutable vectors and functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+macro_rules! vec_of_strings {
+    ($($x:expr),*) => (vec![$($x.to_string()),*]);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut lukes_loves: Vec<String> = vec_of_strings!["Nona", "Samantha", "Lucy", "Charles"];
+    lukes_loves.push("Myself".to_string());
+    print_my_love(lukes_loves);
+}
+
+fn print_my_love(my_loves: Vec<String>) {
+    let string_val: String = my_loves.join(", ");
+    println!("I love {}!", string_val);
 }
