73f5c9ade642e2d3d10dfcb43780073198fe100c
TheMasonX <norrtec@gmail.com>
2023-11-25 18:59:28 -0600

    Moved formatting into a library and added unit tests

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+pub fn format_list(list: Vec<String>) -> String {
+    let love_count: usize = list.len();
+    let string_val: String = match love_count {
+        0 => String::from("nothing"),
+        1 => list.concat().to_string(),
+        2 => format!("{} and {}", list[0], list[1]),
+        _ => format!(
+            "{}, and {}",
+            list[..love_count - 1].join(", "),
+            list[love_count - 1],
+        ),
+    };
+    string_val
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_format_list() {
+        assert_eq!(format_list(vec![]), "nothing");
+        assert_eq!(format_list(vec!["Nona".to_string()]), "Nona");
+        assert_eq!(
+            format_list(vec!["Nona".to_string(), "Samantha".to_string()]),
+            "Nona and Samantha"
+        );
+        assert_eq!(
+            format_list(vec![
+                "Nona".to_string(),
+                "Samantha".to_string(),
+                "Lucy".to_string(),
+                "Charles".to_string()
+            ]),
+            "Nona, Samantha, Lucy, and Charles"
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
 macro_rules! vec_of_strings {
-    ($($x:expr),*) => (vec![$($x.to_string()),*]);
+    ($($x:expr),*) => (vec![$(String::from($x)),*]);
 }
-
-// macro_rules! unwrap_or_unknown {
-//     ($($x:expr)) => {
-//         $x.unwrap_or(String::from("Unknown"))
-//     };
-// }
 
 fn main() {
     let mut lukes_loves: Vec<String> = vec_of_strings!["Nona", "Samantha", "Lucy", "Charles"];
     lukes_loves.push("myself".to_string());
-    print_my_love(lukes_loves);
-}
-
-fn print_my_love(my_loves: Vec<String>) {
-    let string_val: String = match my_loves.len() {
-        0 => "nobody".to_string(),
-        1 => my_loves.concat().to_string(),
-        2 => format!(
-            "{} and {}",
-            my_loves.first().unwrap(),
-            my_loves.last().unwrap(),
-        ),
-        _ => format!(
-            "{}, and {}",
-            my_loves[..my_loves.len() - 1].join(", "),
-            my_loves.last().unwrap(),
-        ),
-    };
-    println!("I love {}!", string_val);
+    println!("I love {}!", test_project::format_list(lukes_loves));
 }
