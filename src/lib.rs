4fed9f8d35b7208bfde65be93d9890346ccbe117
TheMasonX <norrtec@gmail.com>
2023-11-26 16:05:26 -0600

    Added repo link to main document page and MIT license

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 //! Mostly just an exploration of the Rust language and modern CI/CD practices, but I'll use it to include some common/useful utilities as I write them.
+//! <br>
+//! [GitHub Repo](https://github.com/TheMasonX/tmx_utils)
 
 /// String Extensions
 pub mod string_ext;
