f0f7a654fa1daf359575e79ca5a56a51e43dda33
TheMasonX <norrtec@gmail.com>
2023-11-25 19:28:31 -0600

    Added documentation tests to the format_list method

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+/// Formats a list of strings with commas and "and" if necessary.
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(test_project::format_list(
+///            vec![]),
+///            "nothing");
+/// ```
+///
+/// ```
+/// assert_eq!(test_project::format_list(
+///            vec!["Nona".to_string()]),
+///            "Nona");
+/// ```
+///
+/// ```
+/// assert_eq!(test_project::format_list(
+///            vec!["Nona".to_string(),
+///            "Samantha".to_string()]),
+///            "Nona and Samantha");
+/// ```
+///
+/// ```
+/// assert_eq!(test_project::format_list(
+///            vec!["Nona".to_string(),
+///            "Samantha".to_string(),
+///            "Lucy".to_string(),
+///            "Charles".to_string()]),
+///            "Nona, Samantha, Lucy, and Charles");
+/// ```
+///
 pub fn format_list(list: Vec<String>) -> String {
     let love_count: usize = list.len();
     let string_val: String = match love_count {
